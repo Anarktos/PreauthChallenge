6986cbeeb7ee715063dd1814e5956b492cf21b86
Anarktos <camilowhite7@gmail.com>
2022-01-21 17:42:30 -0500

    update game1

--- game1/game-01.ts
+++ game1/game-01.ts
@@ -1,13 +1,13 @@
 let list : number[] = [2, 5, 8, 14, 0, 4, 6 , 7, 1, 10, 11, 12, 13, 15, 16, 17, 20, 22, 45, 68, -1, 40, 22, 66, 41];
 
 function subSet(m, n){
-    let numbers : number[];
+    let numbers = new Set();
     for (let i = 0; i < m.length; i++){
         let a = n - m[i];
-        if (numbers.includes(a)) {
+        if (numbers.has(a)) {            
             return [m[i], a]
-        }
-        numbers.push(m[i]);
+        }        
+        numbers.add(m[i]); 
     }
     return 'cannot find a subset'  
 }
